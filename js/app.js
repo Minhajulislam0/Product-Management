9ecb2a70c11f4d0db7ce99e772d87599f8bce1ff
Minhajulislam0 <mdmishu98730@gmail.com>
2023-11-24 01:26:19 +0600

    Final Product

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNav = document.querySelector('.primary-navigation');
-const primaryHeader = document.querySelector('.primary-header')
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
 
-navToggle.addEventListener('click',() =>{
-    primaryNav.hasAttribute('data-visible')
-    ? navToggle.setAttribute('aria-expended', false)
-    : navToggle.setAttribute('aria-expended', true)
-    primaryNav.toggleAttribute('data-visible')
-    primaryHeader.toggleAttribute('data-overlay')
-})
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expended", false)
+    : navToggle.setAttribute("aria-expended", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
 
 // slider function
-    const slider = new A11YSlider(document.querySelector('.slider'), {
-      adaptiveHeight: true,
-      dots: true,
-      centerMode: true,
-      arrows:false,
-      responsive: {
-        480: {
-          dots: false, // dots enabled 1280px and up
-        }
-      }
-    });
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: true,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
+});
